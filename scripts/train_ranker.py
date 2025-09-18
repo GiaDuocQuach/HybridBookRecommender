f5d226ed726a1843fc7360b4635d9a0d83f74294
Dwight <163412860+GiaDuocQuach@users.noreply.github.com>
2025-09-19 01:40:59 +0700

    Update train_ranker.py

--- scripts/train_ranker.py
+++ scripts/train_ranker.py
@@ -2,7 +2,6 @@ from bookrec.models.train_model import train_and_evaluate
 
 if __name__ == "__main__":
     thr = train_and_evaluate()
-    # Lưu ngưỡng tối ưu để Flask app có thể đọc (nếu cần)
     import os
     os.makedirs("models", exist_ok=True)
     with open("models/optimal_threshold.txt", "w", encoding="utf-8") as f:
